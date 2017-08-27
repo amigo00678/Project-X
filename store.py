78abd8919442aa1a1634403ce098a3c83062202a
Your Name <you@example.com>
2017-08-27 10:55:07 +0400

    Added cron for store update

--- store.py
+++ store.py
@@ -5,18 +5,8 @@ class Product:
 	brand = ForeignKey()
 	group = ForeignKey()
 	discount = DecimalField()
+	actual_price = DecimalField()
 
-	@property
-	def disc_price(self):
-		return self.price * self.discount * 0.01
-
-	@property
-	def discount(self):
-		now = datetime.now()
-		return max(
-			Discount.objects.filter(brand=self.brand, start_date__gte=now, end_date__lte=now).first().size,
-			Discount.objects.filter(group=self.group, start_date__gte=now, end_date__lte=now).first().size,
-			Discount.objects.filter(product=self, start_date__gte=now, end_date__lte=now).first().size())
 
 class Brand:
 	name = CharField()
@@ -51,3 +41,37 @@ class Discount:
 	group = ForeignKey('Group', null=True)
 
 
+'''
+Использовать django-cron для обновления цены на продукт каждую минуту
+'''
+
+from django_cron import CronJobBase, Schedule
+
+
+class MyCronJob(CronJobBase):
+	RUN_EVERY_MINS = 1 # every 2 hours
+
+	schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+	code = 'my_app.my_cron_job'    # a unique code
+
+	def do(self):
+		update_prices()
+
+
+def update_prices():
+	for p in Product.objects.all():
+		brand_disc = Discount.objects.filter(brand=p.brand, start_date__gte=now, end_date__lte=now).first()
+		group_disc = Discount.objects.filter(group=p.group, start_date__gte=now, end_date__lte=now).first()
+		product_disc = Discount.objects.filter(product=p, start_date__gte=now, end_date__lte=now).first()
+
+		disc = max(
+			brand_disc.size if brand_disc else 0,
+			group_disc.size if group_disc else 0,
+			product_disc if product_disc else 0)
+
+		if disc:
+			new_price = p.price * disc * 0.01
+
+			if new_price != p.actual_price:
+				p.actual_price = new_price
+				p.save()
